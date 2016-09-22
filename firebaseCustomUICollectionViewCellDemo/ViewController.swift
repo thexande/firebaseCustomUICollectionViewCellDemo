2719b59a890d56c99bc6eb0b21a4059f68e284a0
Alexander Murphy <amurphy345@gmail.com>
2016-09-22 14:50:09 -0600

    Initial Commit

---
+++ firebaseCustomUICollectionViewCellDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firebaseCustomUICollectionViewCellDemo
+//
+//  Created by Alexander Murphy on 9/22/16.
+//  Copyright © 2016 Alexander Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
